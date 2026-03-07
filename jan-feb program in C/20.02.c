fd73f7113a6330e61b879a2956d4c37569df7462
DevadharshiniS99 <devaaathi57@gmail.com>
2026-03-07 06:36:33 +0530

    day 22 completed in c

---
+++ jan-feb program in C/20.02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int arr[5],i,key;
+
+    printf("Enter 5 numbers:\n");
+    for(i=0;i<5;i++)
+        scanf("%d",&arr[i]);
+
+    printf("Enter number to search: ");
+    scanf("%d",&key);
+
+    for(i=0;i<5;i++){
+        if(arr[i]==key){
+            printf("Found at position %d",i+1);
+            return 0;
+        }
+    }
+
+    printf("Not found");
+}
