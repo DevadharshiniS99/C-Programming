224ba3a7c69c90bf758cdb0f31a4b7927cc18470
DevadharshiniS99 <devaaathi57@gmail.com>
2026-03-07 06:21:03 +0530

    day 14 completed in c

---
+++ jan-feb program in C/13.02.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int num,rev=0,rem,original;
+
+    printf("Enter number: ");
+    scanf("%d",&num);
+
+    original=num;
+
+    while(num!=0){
+        rem=num%10;
+        rev=rev*10+rem;
+        num/=10;
+    }
+
+    if(original==rev)
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
